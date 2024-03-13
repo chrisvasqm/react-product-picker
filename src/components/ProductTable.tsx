9092b2730eb5881134ec483695f86f32028a300d
chrisvasqm <christianvasquezmelo@gmail.com>
2024-03-13 13:21:22 -0400

    Add warning tooltip

--- src/components/ProductTable.tsx
+++ src/components/ProductTable.tsx
@@ -1,4 +1,4 @@
-import { TableContainer, Table, Thead, Tr, Th, Tbody, Td, Input, Button } from "@chakra-ui/react";
+import { TableContainer, Table, Thead, Tr, Th, Tbody, Td, Input, Button, Tooltip } from "@chakra-ui/react";
 import Product from "../models/Product";
 
 interface Props {
@@ -22,12 +22,17 @@ const ProductTable = ({ addedProducts, onDeleteProduct, onInputChange }: Props) 
                 {addedProducts && addedProducts.map(product => <Tr>
                     <Td>{product.name}</Td>
                     <Td isNumeric>{product.quantity}</Td>
-                    <Td><Input
-                        id='toBuy'
-                        type='number'
-                        value={product.toBuy}
-                        onChange={event => onInputChange(product.id, parseInt(event.target.value))}
-                    /></Td>
+                    <Td>
+                        <Tooltip label="To Buy must be lower than Quantity" isDisabled={product.toBuy <= product.quantity}>
+                            <Input
+                                id='toBuy'
+                                type='number'
+                                value={product.toBuy}
+                                onChange={event => onInputChange(product.id, parseInt(event.target.value))}
+                                borderColor={product.toBuy > product.quantity ? 'red' : undefined}
+                            />
+                        </Tooltip>
+                    </Td>
                     <Td><Button onClick={() => onDeleteProduct(product.id)}>Delete</Button></Td>
                 </Tr>)}
             </Tbody>
